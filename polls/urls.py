f364afb4b406e50d97190c9706f42a26c7e84140
Seth Watson <seth.watson1@gmail.com>
2020-07-09 23:25:12 -0700

    django default login admin page running on local server

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 # When user goes to '' root url the index function from views file will run. The root path is named index.
-urlpatterns = {
+urlpatterns = [
     path('', views.index, name='index')
-}
+]
