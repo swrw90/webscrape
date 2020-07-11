db57d936f3aeab212630d992831abec805e08540
Seth Watson <seth.watson1@gmail.com>
2020-07-11 01:32:21 -0700

    Modified view.py to list 5 questions by publication date.

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
 
-# When user goes to '' root url the index function from views file will run. The root path is named index.
+# Each path function returns a response matching the url input. <int:question_id> is used to dynamically match any pattern input.
 urlpatterns = [
-    path('', views.index, name='index')
+    # /polls
+    path('', views.index, name='index'),
+    # /polls/5
+    path('<int:question_id>/', views.detail, name='detail'),
+    # /polls/5/results 
+    path('<int:question_id>/results/', views.results, name='results'),
+    # /polls/5/vote
+    path('<int:question_id>/vote/', views.vote, name='vote')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
-# .
+
+# Each view takes a request object and question_id to lookup a specific question using its primary key.
 def index(request):
-    return HttpResponse("Hello, World! You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-publication_date')[:5] #Reverse order list of 5 questions by publication date.
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def  results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)  
