5c61fd0f7ab57c43fa3b5cf6f589568e4c79d450
Seth Watson <seth.watson1@gmail.com>
2020-07-12 20:06:51 -0700

    modified views.py detail function to get question by its primary key or raise 404

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Question
 
 
 # Each view takes a request object and question_id to lookup a specific question using its primary key.
+# Load the index.html and use context dictionary key  to pass the list of questions to the frontend.
 def index(request):
-    latest_question_list = Question.objects.order_by('-publication_date')[:5] #Reverse order list of 5 questions by publication date.
-    template = loader.get_template('polls/index.html')
+    latest_question_list = Question.objects.order_by('-publication_date')[:5] #Reverse order list of 5 questions by publication date to get most recent.
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html/', context) #Sends request and context to index.html
 
+# Try to get question by its primary key and pass it to details.html, otherwise raise 404 error. 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id) #primary key = question_id
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist.")
+    return render(request, 'polls/details.html', {'question': question})
 
 def  results(request, question_id):
     response = "You're looking at the results of question %s."
